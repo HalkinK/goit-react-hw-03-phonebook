0f1c4313bbdaa22804a7ebf41b560c31c4d9fdd7
Konstantin Halkin <constantingal@gmail.com>
2021-07-22 13:15:03 +0300

    контакты сохраняются в локальное хранилище

--- src/App.js
+++ src/App.js
@@ -15,6 +15,21 @@ class App extends React.Component {
     filter: "",
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   onFormSubmit = ({ name, number }) => {
     const contact = { id: shortid.generate(), name, number };
     const { contacts } = this.state;
